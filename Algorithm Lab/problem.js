a59933b48cbebc9081cc1feb04f638c0b5df5ed2
mdruhankarim <mdruhankarim@gmail.com>
2025-02-10 13:11:42 +0600

    all problems solved

--- Algorithm Lab/problem.js
+++ Algorithm Lab/problem.js
@@ -3,7 +3,16 @@ var numbers = [1, 5, 6, 23, 78, 4, 7, 45, 8, 44];
 // find the bigest number from this array
 //   get the sum of the array
 // just showing even number from this array
-
+for(let i=0; i<numbers.length; i++){
+    if(numbers[i]>numbers[i+1]){
+        var temp=numbers[i];
+        numbers[i]=numbers[i+1];
+        numbers[i+1]=temp;
+    }
+    if(i==numbers.length-1){
+        console.log("The biggest number is:"+numbers[i]);
+    }
+}
 var sum=0;
 for(let i=0; i<numbers.length; i++){
     sum+=numbers[i];
